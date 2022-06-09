8676e87b624f0cfe2607380630ed2dac07b2187e
Kishan Khatrani <38881285+kkpkishan@users.noreply.github.com>
2022-06-09 16:10:09 +0530

    Create ubuntu-ami.sh

---
+++ ubuntu-ami.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ "$1" == "" ]; then
+
+  REGIONS=$(aws ec2 describe-regions | jq -r '.Regions[].RegionName')
+
+  for region in $REGIONS
+    do
+		  AMI=$(aws ec2 describe-images \
+		    --region $region \
+		    --filters 'Name=name,Values=ubuntu/images/hvm-ssd/ubuntu-jammy-22.??-amd64-server-2022????' 'Name=state,Values=available' \
+		    --output json | \
+		    jq -r '.Images | sort_by(.CreationDate) | last(.[]).ImageId')
+		  echo -e "    $region: {AMI: $AMI}"
+    done
+
+else
+  AMI=$(aws ec2 describe-images \
+    --region $1 \
+    --owners aws-marketplace \
+    --filters 'Name=name,Values=ubuntu/images/hvm-ssd/ubuntu-jammy-22.??-amd64-server-2022????' 'Name=state,Values=available' \
+    --output json | \
+    jq -r '.Images | sort_by(.CreationDate) | last(.[]).ImageId')
+  echo -e "$1: {AMI: $AMI}"
+fi
